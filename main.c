25dd9b8e173cf651a7ae93e1df8d79ac916215ac
ribocur <ribocur@proton.me>
2024-07-06 14:24:21 +0200

    Add colors

--- main.c
+++ main.c
@@ -8,9 +8,23 @@
 #include <stdlib.h>
 #include <SDL2/SDL_ttf.h>
 
-
 #define _COLOR(RED, GREEN, BLUE)    { RED, GREEN, BLUE, 0xFF }
+#define COLOR(RED, GREEN, BLUE)     ((SDL_Color) { RED, GREEN, BLUE, 0xFF })
+
+const SDL_Color NO_COLOR            = {0, 0, 0, 0};
+const SDL_Color COLOR_BLACK         = _COLOR(0, 0, 0);
 const SDL_Color COLOR_WHITE         = _COLOR(0xFF, 0xFF, 0xFF);
+const SDL_Color COLOR_GRAY          = _COLOR(0x64, 0x64, 0x64);
+const SDL_Color COLOR_DARK_GRAY     = _COLOR(0x1E, 0x1E, 0x1E);
+const SDL_Color COLOR_LIGHT_GRAY    = _COLOR(0xC8, 0xC8, 0xC8);
+const SDL_Color COLOR_RED           = _COLOR(0xF5, 0x3B, 0x56);
+const SDL_Color COLOR_GREEN         = _COLOR(0x01, 0x9F, 0x13);
+const SDL_Color COLOR_BLUE          = _COLOR(0x38, 0x95, 0xD3);
+const SDL_Color COLOR_YELLOW        = _COLOR(0xF7, 0xDC, 0x11);
+const SDL_Color COLOR_ORANGE        = _COLOR(0xFF, 0x85, 0);
+const SDL_Color COLOR_PINK          = _COLOR(0xFF, 0, 0xCE);
+const SDL_Color COLOR_VIOLET        = _COLOR(0x91, 0, 0xFF);
+
 
 #define SCREEN_WIDTH 1920
 #define SCREEN_HEIGHT 1080
@@ -26,6 +40,19 @@ typedef struct
 	// 2 = was pressed
 	short int state;
 } Box;
+
+void setup_standard_box()
+{
+	// Surface to render text onto
+}
+void setup_standard_box_pressed()
+{
+	
+}
+void setup_standard_box_was_pressed()
+{
+	
+}
 
 int main()
 {
@@ -78,13 +105,13 @@ while (!close)
 				break;
 			case SDL_KEYDOWN:
 				// Debuging
-				printf("KeyDown:");
+				printf("KeyDown: ");
 				printf(SDL_GetScancodeName(event.key.keysym.scancode), "%s");
 				printf("\n");
 				fflush(stdout);
 				break;
 			case SDL_KEYUP:
-				printf("KeyUp");
+				printf("KeyUp: ");
 				printf(SDL_GetScancodeName(event.key.keysym.scancode), "%s");
 				printf("\n");
 				fflush(stdout);
